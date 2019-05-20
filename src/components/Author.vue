084db192713b4c604cfa3d5830c798cfc9e4bdd5
calebanthony <caleb.m.anthony@gmail.com>
2019-05-20 10:03:38 -0600

    Update tagline on home page to match README

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -10,9 +10,7 @@
       {{ $static.metaData.siteName }}
     </h1>
 
-    <p>
-      A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
-    </p>
+    <p>A baseline Gridsome starter to get you going with Bulma.</p>
   </div>
 </template>
 
